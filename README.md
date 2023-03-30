22c5d65f3a85ae35781c0475edeeb5f96d67b332
Mohammad Javad Zare <mjz589.2018@gmail.com>
2023-03-30 20:22:29 +0330

    v1.0

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Lyrics-Selenium-Python
  just input the singer's name and name of the song and you can read the lyric of music form txt file
+
+it serachs the song in google (Lyrics tab)
---
+++ project.py
@@ -0,0 +1,23 @@
+from selenium.webdriver import Chrome
+from selenium.webdriver.common.keys import Keys
+from bs4 import BeautifulSoup
+
+driver = Chrome(executable_path= "chromedriver.exe")
+driver.get("https://www.google.com/")
+search_bar = driver.find_element('name' , 'q')
+singer =  input("What's the singer's name? ")
+music = input("What's the name of song? ")
+search_bar.send_keys(f"{singer} {music} Lyrics")
+search_bar.send_keys(Keys.ENTER)
+
+soup = BeautifulSoup(driver.page_source , 'html.parser')
+soup = soup.find_all('div' , attrs= {"jsname" : "WbKHeb"})
+
+for soup1 in soup:
+    soup1 = soup1.find_all('div' , attrs={"jsname" : "U8S5sf"})
+    for div in soup1:
+        div = div.find_all('span' , attrs={"jsname" : "YS01Ge"})
+        with open(f"{singer}-{music} Lyrics.txt" , 'a') as file:
+            for span in div:
+                print(span.text)
+                file.write(span.text + '\n')
